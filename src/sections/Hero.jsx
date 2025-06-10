2b9c0982336810dae7157eff6a5c341d72c04235
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-10 09:59:31 +0545

    add Hero 3d Model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,8 @@ import gsap from "gsap";
 //import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import { words } from "../constants";
-// import HeroExperience from "../components/models/hero_models/HeroExperience";
+import HeroExperience from "../components/Hero_models/HeroExperience";
+
 
 const Hero = () => {
     useGSAP(() => {
@@ -66,7 +67,7 @@ const Hero = () => {
                 {/* RIGHT: 3D Model or Visual */}
                 <figure>
                     <div className="hero-3d-layout">
-                        {/* <HeroExperience /> */}
+                        <HeroExperience />
                     </div>
                 </figure>
             </div>
