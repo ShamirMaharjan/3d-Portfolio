c2e0a2d252cfffc14b525a6e5b19d31b01c1025f
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-10 10:22:21 +0545

    add logo showcase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './sections/Hero'
 import AppShowcase from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
+import LogoShowcase from './sections/LogoShowcase'
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <AppShowcase />
+            <LogoShowcase />
         </div>
     )
 }
