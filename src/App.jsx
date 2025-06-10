233076e5ff63d26b9f555263b9304812aebe35f4
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-10 10:24:41 +0545

    add feature card section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import AppShowcase from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
 import LogoShowcase from './sections/LogoShowcase'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Hero />
             <AppShowcase />
             <LogoShowcase />
+            <FeatureCards />
         </div>
     )
 }
