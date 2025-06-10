d31536aa41348c7528680a2b99dacda8348333c5
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-10 10:10:43 +0545

    add app showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import AppShowcase from './sections/ShowcaseSection'
 
 const App = () => {
     return (
         <div>
             <Hero />
+            <AppShowcase />
         </div>
     )
 }
